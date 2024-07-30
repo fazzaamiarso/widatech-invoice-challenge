f52b6da1137ca0a8d9813cd056a1c565795585f4
fazzaamiarso <fazzaamiarso@gmail.com>
2024-07-31 06:57:07 +0700

    chore(FE): layout create invoice sheet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,118 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "./components/ui/textarea";
+
+const createInvoiceSchema = z.object({
+  customer: z.string().min(1, { message: "Field is required!" }).max(50),
+  salesperson: z.string().min(1, { message: "Field is required!" }).max(50),
+  notes: z.string().optional(),
+});
+
+type FormValues = z.infer<typeof createInvoiceSchema>;
+
 function App() {
-  return <div className="text-xl font-bold">Hello world</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const form = useForm<FormValues>({
+    resolver: zodResolver(createInvoiceSchema),
+    defaultValues: {
+      customer: "",
+      salesperson: "",
+      notes: "",
+    },
+  });
+
+  const onSubmit = (values: FormValues) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <header className="">
+        <div className="mx-auto flex w-11/12 justify-between py-4">
+          <h1>Invoice POS</h1>
+          <Button onClick={() => setIsOpen(true)}>Create Invoice</Button>
+        </div>
+      </header>
+      <main>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Create New Invoice</SheetTitle>
+            </SheetHeader>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="customer"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Customer Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="salesperson"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Salesperson Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="notes"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Notes</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Type your message here."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit">Create</Button>
+              </form>
+            </Form>
+          </SheetContent>
+        </Sheet>
+      </main>
+    </>
+  );
 }
 
 export default App;
