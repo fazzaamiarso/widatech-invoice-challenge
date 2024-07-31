abcf9f6a51c6eb47ffc5841bd775392710faeb8e
fazzaamiarso <fazzaamiarso@gmail.com>
2024-08-01 00:46:04 +0700

    chore(BE): update schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,9 +1,12 @@
+import { relations } from "drizzle-orm";
 import {
   bigserial,
   pgTable,
   text,
   timestamp,
   varchar,
+  integer,
+  bigint,
 } from "drizzle-orm/pg-core";
 
 export const invoices = pgTable("invoice", {
@@ -11,5 +14,38 @@ export const invoices = pgTable("invoice", {
   date: timestamp("date").defaultNow().notNull(),
   notes: text("notes"),
   customer: varchar("customer_name", { length: 50 }).notNull(),
-  salesperson: varchar("customer_name", { length: 50 }).notNull(),
+  salesperson: varchar("salesperson_name", { length: 50 }).notNull(),
 });
+
+// Simplified version of the hardcoded data in Frontend
+export const products = pgTable("product", {
+  id: bigserial("id", { mode: "number" }).primaryKey(),
+  name: varchar("name", { length: 50 }),
+  price: integer("price"),
+});
+
+export const invoiceItem = pgTable("invoice_item", {
+  id: bigserial("id", { mode: "number" }).primaryKey(),
+  invoiceId: bigint("invoice_id", { mode: "number" }).notNull(),
+  productId: bigint("product_id", { mode: "number" }).notNull(),
+  quantity: integer("quantity").notNull(),
+});
+
+export const invoiceItemRelation = relations(invoiceItem, ({ one }) => ({
+  invoice: one(invoices, {
+    fields: [invoiceItem.invoiceId],
+    references: [invoices.id],
+  }),
+  product: one(products, {
+    fields: [invoiceItem.productId],
+    references: [products.id],
+  }),
+}));
+
+export const productsRelation = relations(products, ({ many }) => ({
+  invoiceItems: many(invoiceItem),
+}));
+
+export const invoicesRelation = relations(invoices, ({ many }) => ({
+  invoiceItems: many(invoiceItem),
+}));
--- backend/src/db/seed.ts
+++ backend/src/db/seed.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 const { Pool } = pg;
-import { salespersons } from "./schema";
+import { invoiceItem, invoices, products } from "./schema";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -16,16 +16,45 @@ const main = async () => {
   const db = drizzle(client);
 
   console.log("🌱 SEEDING START");
-  // try {
-  //   await db
-  //     .insert(salespersons)
-  //     .values([{ name: "James" }, { name: "Elon" }, { name: "Kamala" }])
-  //     .returning();
-  // } catch (e: any) {
-  //   console.log(e?.message ?? "Something went wrong!");
-  // } finally {
-  //   await client.end();
-  // }
+  try {
+    console.log("RESET DATABASE START");
+    await db.delete(products);
+    await db.delete(invoiceItem);
+    await db.delete(invoices);
+    console.log("RESET DATABASE END");
+
+    await Promise.all([
+      db.insert(products).values([
+        { id: 345, name: "primogems", price: 30 },
+        { id: 222, name: "Geo Sigil", price: 20 },
+        { id: 333, name: "Anemo Sigil", price: 10 },
+      ]),
+      db.insert(invoices).values([
+        {
+          id: 1,
+          customer: "Aether",
+          salesperson: "Katheryne",
+          notes: "Lorem ipsum",
+        },
+        {
+          id: 2,
+          customer: "Diluc",
+          salesperson: "Katheryne",
+          notes: "Lorem ipsum",
+        },
+      ]),
+    ]);
+
+    await db.insert(invoiceItem).values([
+      { quantity: 2, invoiceId: 1, productId: 333 },
+      { quantity: 1, invoiceId: 1, productId: 222 },
+      { quantity: 1, invoiceId: 2, productId: 345 },
+    ]);
+  } catch (e: any) {
+    console.log(e?.message ?? "Something went wrong!");
+  } finally {
+    await client.end();
+  }
   console.log("🏁 SEEDING END");
 };
 
