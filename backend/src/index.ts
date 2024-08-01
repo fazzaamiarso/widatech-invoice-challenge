36c32d48611ca3de26ea1fe9050129ff83f0e200
fazzaamiarso <fazzaamiarso@gmail.com>
2024-08-02 06:09:19 +0700

    feat(BE): implements logger with morgan

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import dotenv from "dotenv";
-import invoiceRouter from "./routes/invoices";
+import { invoiceRouter } from "./routes";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 dotenv.config();
 
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,3 +1,3 @@
 import invoiceRouter from "./invoices";
 
-export default { invoiceRouter };
+export { invoiceRouter };
