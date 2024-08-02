bd71df773bc2a6e8acb7a1fbd6b664102bd68413
fazzaamiarso <fazzaamiarso@gmail.com>
2024-08-02 07:57:06 +0700

    chore(BE): add variability to seed script

--- backend/src/db/seed.ts
+++ backend/src/db/seed.ts
@@ -9,19 +9,51 @@ dotenv.config();
 if (!("DATABASE_URL" in process.env))
   throw new Error("DATABASE_URL not found on .env.development");
 
+const generateRandomIdxFromArray = (array: any[]) => {
+  return Math.floor(Math.random() * array.length);
+};
+
 const main = async () => {
   const client = new Pool({
     connectionString: process.env.DATABASE_URL,
   });
   const db = drizzle(client);
 
-  console.log("🌱 SEEDING START");
   try {
     console.log("RESET DATABASE START");
     await db.delete(products);
     await db.delete(invoiceItem);
     await db.delete(invoices);
     console.log("RESET DATABASE END");
+
+    console.log("🌱 SEEDING START");
+
+    const salespersons = ["Nahida", "Furina", "Arlecchino", "Venti"];
+    const customers = [
+      "Aether",
+      "Diluc",
+      "Zhongli",
+      "Childe",
+      "Navia",
+      "Bennet",
+      "HuTao",
+      "Kazuha",
+    ];
+    const generatedInvoices = Array.from({ length: 30 }).map((_, idx) => {
+      const today = new Date();
+      const date = new Date();
+      date.setDate(today.getDate() - idx);
+
+      return {
+        id: idx,
+        customer: customers[generateRandomIdxFromArray(customers)],
+        salesperson: salespersons[generateRandomIdxFromArray(salespersons)],
+        notes: "lorem ipsum",
+        date,
+      };
+    });
+
+    const productsId = [111, 222, 333, 444, 555];
 
     await Promise.all([
       db.insert(products).values([
@@ -31,43 +63,18 @@ const main = async () => {
         { id: 444, name: "Hydro Sigil", price: 40 },
         { id: 555, name: "Dendro Sigil", price: 55 },
       ]),
-      db.insert(invoices).values([
-        {
-          id: 1,
-          customer: "Aether",
-          salesperson: "Katheryne",
-          notes: "Lorem ipsum",
-        },
-        {
-          id: 2,
-          customer: "Diluc",
-          salesperson: "Katheryne",
-          notes: "Lorem ipsum",
-        },
-        {
-          id: 3,
-          customer: "Nahida",
-          salesperson: "Timmy",
-          notes: "Lorem ipsum",
-        },
-        {
-          id: 4,
-          customer: "Arlecchino",
-          salesperson: "Timmy",
-          notes: "Lorem ipsum",
-          date: new Date("2024-07-22"),
-        },
-      ]),
+      db.insert(invoices).values(generatedInvoices),
     ]);
 
-    await db.insert(invoiceItem).values([
-      { quantity: 2, invoiceId: 1, productId: 333 },
-      { quantity: 1, invoiceId: 1, productId: 222 },
-      { quantity: 1, invoiceId: 2, productId: 111 },
-      { quantity: 4, invoiceId: 3, productId: 555 },
-      { quantity: 2, invoiceId: 3, productId: 111 },
-      { quantity: 10, invoiceId: 4, productId: 222 },
-    ]);
+    await Promise.all(
+      generatedInvoices.map((item) => {
+        return db.insert(invoiceItem).values({
+          invoiceId: item.id,
+          productId: productsId[generateRandomIdxFromArray(productsId)],
+          quantity: 2,
+        });
+      })
+    );
   } catch (e: any) {
     console.log(e?.message ?? "Something went wrong!");
   } finally {
